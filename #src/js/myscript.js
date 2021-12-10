317666aed686381e1d51c67f498c562dc94f6c6d
Web-romans <roma88gruzd88@mail.ru>
2021-12-10 13:24:29 +0300

    add section choose-plan, card, refactoring

--- #src/js/myscript.js
+++ #src/js/myscript.js
@@ -13,10 +13,41 @@ headerBurger.addEventListener('click', function () {
 
 /* Меню фикс при ресайзе */
 window.addEventListener('resize', function (event) {
-    if (window.innerWidth >= 980) {
+    mobileSlider();
+    if (window.innerWidth >= 1260) {
         headerBurger.classList.remove("close");
         headerMenu.classList.remove("open");
         overlay.classList.remove("owerlay");
         btns.classList.remove("mobile");
     }
 }, true);
+
+
+const mSlider = document.querySelector(".card-slider");
+
+let cardSlider;
+
+function mobileSlider() {
+    if (window.innerWidth <= 1260 && mSlider.dataset.mobile == "false") {
+        cardSlider = new Swiper(mSlider, {
+            centeredSlides: true,
+            spaceBetween: 30,
+            slidesPerView: 'auto',
+            loop: true,
+            loopedSlides: 20,
+            slideClass: "card"
+        });
+    }
+
+    mSlider.dataset.mobile = "true";
+
+    if (window.innerWidth > 1260) {
+        mSlider.dataset.mobile = "false";
+
+        if (mSlider.classList.contains("swiper-initialized")) {
+            cardSlider.destroy();
+        }
+    }
+}
+
+mobileSlider();
